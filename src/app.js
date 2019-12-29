0b931681c2cb289d3ef39b1e14d740c4faa35a14
Valeriia <valeria.chyornaya@gmail.com>
2019-12-29 11:30:09 +0200

    Homework12 commit-2

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
 import debounce from 'lodash.debounce';
 
-import {PNotify, pWarning} from './utils/pnotify';
-import {getCountry} from './services/api';
-import {refs} from './utils/refs';
-
+import { PNotify, pWarning } from './utils/pnotify';
+import { getCountry } from './services/api';
+import { refs } from './utils/refs';
+import templateList from './utils/templates/list-item.hbs';
+import templateCard from './utils/templates/country-description.hbs';
 
 //getCountry('ukr').then(data => console.log(data));
 
-refs.input.addEventListener('input',debounce(getInputValue,500));
+refs.input.addEventListener('input', debounce(getInputValue, 500));
 
-function getInputValue (event){
-    event.preventDefault();
-    const {target} = event;
-    const outputData = getCountry(target.value).length;
-    console.log(outputData);
-    
-    //pWarning(target.value);
+function getInputValue(event) {
+  event.preventDefault();
+  const { target } = event;
+  if (event.target.value === "") {
+    return;
+  }
+  getCountry(target.value).then(country => {
+    if (country.length === 1) {
+      updateHtml(`${templateCard(country)}`);
+    }
+    if(country.length > 1 & country.length <= 10){
+        updateHtml(`${templateList(country)}`);
+    }
+    if(country.length > 10){
+        pWarning();
+    }
+    //else{pWarning('Sorry, we can\'t find, try again!')};
+  }).catch(error => console.error(error));;
 }
 
-
-function updateHtml (data){
-    refs.output.insertAdjacentHTML('afterbegin', data);
+function updateHtml(data) {
+  refs.output.insertAdjacentHTML('afterbegin', data);
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ const URL = 'https://restcountries.eu/rest/v2/name/';
 
 const getCountry = country =>
   fetch(URL + country)
-    .then(response => console.log(response.json()))
-   //.then(data => console.log(JSON.stringify(data)))
+    .then(response => response.json())
+  // .then(country => Array.from(country))
     .catch(console.log('Error!'));
 
 export { getCountry };
